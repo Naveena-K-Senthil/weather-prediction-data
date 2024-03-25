657c0aa93f94cfd7b64fe5af7292e832a3dd23c9
nasenthi_cisco <nasenthi@cisco.com>
2024-03-25 11:48:54 +0530

    change csv

--- get_data_test.py
+++ get_data_test.py
@@ -4,7 +4,7 @@ from kfp.components import OutputPath
 def get_data_test(output_path: OutputPath()):
     import urllib.request
     print("starting download...")
-    url = "https://raw.githubusercontent.com/Naveena-K-Senthil/weather-prediction-data/main/jfk_weather.csv"
+    url = "https://raw.githubusercontent.com/Naveena-K-Senthil/weather-prediction-data/main/data/jfk_weather.csv"
     urllib.request.urlretrieve(url, output_path)
     print("done")
 
